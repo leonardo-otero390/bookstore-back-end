41957375ae5d663f7255ea64619370ef690a4415
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-10 09:50:07 -0400

    feat: logout done

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import { ObjectId } from 'mongodb';
 import { v4 as uuid } from 'uuid';
 import db from '../database/connection.js';
 
@@ -18,6 +19,16 @@ export async function upsert(req, res) {
         { upsert: true }
       );
     return res.status(200).send({ token, name: user.hashedPassword });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+export async function remove(req, res) {
+  const userId = new ObjectId(res.locals.userId);
+  try {
+    await db.collection('sessions').deleteMany({ userId });
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,10 +4,12 @@ import createUser from '../controllers/userController.js';
 import * as sessionsController from '../controllers/sessionsController.js';
 import logInSchemaValidation from '../middlewares/loginSchemaValidation.js';
 import userSchemaValidation from '../middlewares/userSchemaValidation.js';
+import validateToken from '../middlewares/tokenValidator.js';
 
 const userRouter = Router();
 
 userRouter.post('/sign-up', userSchemaValidation, createUser);
 userRouter.post('/log-in', logInSchemaValidation, sessionsController.upsert);
+userRouter.delete('/sessions', validateToken, sessionsController.remove);
 
 export default userRouter;
