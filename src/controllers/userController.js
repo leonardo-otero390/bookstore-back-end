bd773ed1877316a64fb1d9f364de14888e135a4e
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-02-11 21:53:00 -0300

    feat: do checkout without checks

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import dayjs from 'dayjs';
 
 import db from '../database/connection.js';
 
@@ -25,5 +26,61 @@ export async function createUser(req, res) {
     res
       .status(500)
       .send('Houve uma falha ao cadastrar usuário. Por favor, tente novamente');
+  }
+}
+
+function valueOfItems(items) {
+  let value = 0.0;
+
+  items.forEach((item) => {
+    value += item.quantityInCart * item.price;
+  });
+
+  return value;
+}
+
+export async function registerPurchase(req, res) {
+  console.log(req.body);
+
+  const newPurchase = {};
+  newPurchase.items = req.body;
+
+  try {
+    const { userId } = res.locals;
+
+    const usersCollection = db.collection('users');
+
+    const user = await usersCollection.findOne({ _id: userId });
+
+    newPurchase.value = valueOfItems(newPurchase.items);
+    newPurchase.date = dayjs().format('DD/MM/YYYY');
+
+    const updatedPurchases = user.purchases;
+    updatedPurchases.push(newPurchase);
+
+    const productsCollection = db.collection('products');
+
+    for (let i = 0; i < newPurchase.items.length; i += 1) {
+      const item = newPurchase.items[i];
+
+      await productsCollection.updateOne(
+        { title: item.title },
+        { $inc: { quantity: -item.quantityInCart } }
+      );
+    }
+
+    await usersCollection.updateOne(
+      { _id: userId },
+      { $set: { purchases: updatedPurchases } }
+    );
+
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        'Houve uma falha ao finalizar a compra. Por favor, tente novamente'
+      );
   }
 }
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import { createUser } from '../controllers/userController.js';
+import { createUser, registerPurchase } from '../controllers/userController.js';
 import userSchemaValidation from '../middlewares/userSchemaValidation.js';
+import validateToken from '../middlewares/tokenValidator.js';
 
 const userRouter = Router();
 
 userRouter.post('/sign-up', userSchemaValidation, createUser);
+userRouter.post('/checkout', validateToken, registerPurchase);
 
 export default userRouter;
