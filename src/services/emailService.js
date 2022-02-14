ab2dd8ff506a943bb537978ca0c20cf709eb0b23
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-14 17:31:03 -0400

    feat: sendMessage function

---
+++ src/services/emailService.js
@@ -0,0 +1,22 @@
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export async function send({ email, content }) {
+  const msg = {
+    to: email,
+    from: 'leonardo.otero390@gmail.com',
+    subject: 'Sua compra na bookstore foi efetuada',
+    text: content,
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
+    throw new Error();
+  }
+}
